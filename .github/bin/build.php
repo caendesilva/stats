34fcce06ae3ddd02c64f6b2a0a21af4b9aace5e6
Caen De Silva <caen@desilva.se>
2024-06-23 11:10:29 +0200

    Convert the YAML to JSON

--- .github/bin/build.php
+++ .github/bin/build.php
@@ -1,3 +1,13 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+$yaml = file_get_contents(__DIR__ . '/../../data.yml');
+$data = Symfony\Component\Yaml\Yaml::parse($yaml);
+
+echo "Data loaded from 'data.yml':\n";
+
+$json = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents(__DIR__ . '/../../data.json', $json);
+
+echo "Data saved to 'data.json':\n";

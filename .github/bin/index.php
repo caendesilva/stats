074f7138bec43d232704b8dacc22b90d2ef483c1
Caen De Silva <caen@desilva.se>
2024-06-23 12:18:20 +0200

    Cleanup and improve formatting

--- .github/bin/index.php
+++ .github/bin/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Statistics API</title>
-    <style>/** https://gist.github.com/caendesilva/619cd1db565aed5bdbadf2a8d73d92c8 */body{font-family:Arial,sans-serif;line-height:1.6;margin:0;padding:40px 20px}main{max-width:800px;margin:0 auto;padding:20px;border:1px solid #ddd;border-radius:5px;background-color:#f9f9f9}article{margin:0 auto}h1{font-size:2em;margin-bottom:.5em}h2{font-size:1.5em;margin-bottom:.5em}h3{font-size:1.17em;margin-bottom:.5em}h4{font-size:1em;margin-bottom:.5em}h5{font-size:.83em;margin-bottom:.5em}h6{font-size:.67em;margin-bottom:.5em}a{color:#007bff;text-decoration:none}a:hover{text-decoration:underline}article h1:first-of-type{margin-top:0}</style>
+    <style>/** https://gist.github.com/caendesilva/619cd1db565aed5bdbadf2a8d73d92c8 */body{font-family:Arial,sans-serif;line-height:1.6;margin:0;padding:40px 20px}main{max-width:800px;margin:0 auto;padding:20px;border:1px solid #ddd;border-radius:5px;background-color:#f9f9f9}article{margin:0 auto}h1{font-size:1.5em;margin-bottom:.5em}h2{font-size:1.2em;margin-bottom:.25em}a{color:#007bff;text-decoration:none}a:hover{text-decoration:underline}article h1:first-of-type{margin-top:0}ul{margin-top: 0.5em;}footer p{margin: 0;}</style>
 </head>
 <body>
     <main>
@@ -15,11 +15,12 @@
             <h2>Links</h2>
             <ul>
                 <li><a href="data.json">JSON API</a></li>
-                <li><a href="https://github.com/caendesilva/stats">GitHub repository</a></li>
             </ul>
         </article>
-
+        <hr style="opacity: 0.5; max-width: 600px; margin-left: 0;">
         <footer>
+            <h2>About</h2>
+            <p>See the source code on the <a href='https://github.com/caendesilva/stats'>GitHub repository</a></p>
             <p>Generated on: <?php echo date('Y-m-d H:i:s'); ?></p>
         </footer>
     </main>

bdc4718abbeb3db5600e48a56dfb2a2de353d44e
Caen De Silva <caen@desilva.se>
2024-06-23 11:57:08 +0200

    Revert "Add command prefix"
    This reverts commit 7a795b692b80f128790e623378c7ef37afb24841.

--- .github/bin/build.php
+++ .github/bin/build.php
@@ -21,7 +21,7 @@ if (! in_array($function, $commands)) {
 
 exit(call_user_func($function) ?: 0);
 
-function command_lint(): int
+function lint(): int
 {
     $yaml = file_get_contents('php://stdin');
 
@@ -38,7 +38,7 @@ function command_lint(): int
     return 0;
 }
 
-function command_convert(): int
+function convert(): int
 {
     try {
         $yaml = file_get_contents('php://stdin');
@@ -68,7 +68,7 @@ function command_convert(): int
     return 0;
 }
 
-function command_build(): int
+function build(): int
 {
     try {
         mkdir(BASE_PATH.'/site');

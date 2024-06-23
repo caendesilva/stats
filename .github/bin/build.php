7681b6530456549673983d6797e37bcc07ace8d7
Caen De Silva <caen@desilva.se>
2024-06-23 11:14:13 +0200

    Get function to call from first argument

--- .github/bin/build.php
+++ .github/bin/build.php
@@ -2,12 +2,32 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$yaml = file_get_contents(__DIR__ . '/../../data.yml');
-$data = Symfony\Component\Yaml\Yaml::parse($yaml);
+// Get function to call from first argument
 
-echo "Data loaded from 'data.yml':\n";
+$function = $argv[1] ?? null;
 
-$json = json_encode($data, JSON_PRETTY_PRINT);
-file_put_contents(__DIR__ . '/../../data.json', $json);
+$commands = ['convert'];
 
-echo "Data saved to 'data.json':\n";
+if (! in_array($function, $commands)) {
+    if ($function) {
+        echo "Unknown command: $function\n";
+    }
+    echo "Available commands:\n";
+    foreach ($commands as $command) {
+        echo "  $command\n";
+    }
+    exit(1);
+}
+
+function convert(): void
+{
+    $yaml = file_get_contents(__DIR__ . '/../../data.yml');
+    $data = Symfony\Component\Yaml\Yaml::parse($yaml);
+
+    echo "Data loaded from 'data.yml':\n";
+
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents(__DIR__ . '/../../data.json', $json);
+
+    echo "Data saved to 'data.json':\n";
+}

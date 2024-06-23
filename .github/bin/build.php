214d490996f7a79b1be4837bab21f12324e50587
Caen De Silva <caen@desilva.se>
2024-06-23 11:08:04 +0200

    Validate file exists

--- .github/bin/build.php
+++ .github/bin/build.php
@@ -1,3 +1,8 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+if (! file_exists(__DIR__ . '/data.yml')) {
+    echo "File 'data.yml' not found, did you run from the root of the repository?";
+    exit(1);
+}
